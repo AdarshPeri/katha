476b316426bcf16cade966c507011bfbca74a3e4
adarshperi <adarshperi@gmail.com>
2024-04-04 14:16:23 +0530

    creds for DB changed

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://vjvrivtovosplhovmvfs.supabase.co';
+export const supabaseUrl = 'https://syqtfcaljarcgeywfeki.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqdnJpdnRvdm9zcGxob3ZtdmZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMDQyOTM4NiwiZXhwIjoyMDI2MDA1Mzg2fQ.a66kom_TRjz5KsDQS2APQYrCNc2JyBB8D-ekdRmacuQ';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5cXRmY2FsamFyY2dleXdmZWtpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjIxODMzOSwiZXhwIjoyMDI3Nzk0MzM5fQ.3f2r9mE1gOUKETS1IGT60_dKKs8b2Gmm3ENMF2LBUAs';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
